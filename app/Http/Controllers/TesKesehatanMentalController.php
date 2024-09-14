01782c444ca72f52f23c02a539f5c8f187a9eb8d
noppiii <mnoviantoanggoro@gmail.com>
2024-09-14 12:19:41 +0700

    post data answer

--- app/Http/Controllers/TesKesehatanMentalController.php
+++ app/Http/Controllers/TesKesehatanMentalController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\MentalHealthAnswerModel;
+use App\Models\MentalHealthOptionModel;
+use App\Models\MentalHealthResultModel;
 use App\Models\MentalHealthTestModel;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TesKesehatanMentalController extends Controller
 {
@@ -38,10 +42,46 @@ class TesKesehatanMentalController extends Controller
 
     public function submitTest(Request $request, $idTest, $nama)
     {
-        // Use dd to inspect the request data
-        dd($request->all());
+        try {
+            $mahasiswaId = Auth::guard('mahasiswa')->user()->id;
+//            dd($mahasiswaId);
+            $totalScore = 0;
 
-        // Your existing code for handling the form submission
+            $result = MentalHealthResultModel::create([
+                'mahasiswa_id' => $mahasiswaId,
+                'mental_health_test_id' => $idTest,
+                'total_score' => 0,
+            ]);
+
+            foreach ($request->all() as $key => $value) {
+                if (strpos($key, 'question_') === 0) {
+                    $questionId = str_replace('question_', '', $key);
+
+                    $option = MentalHealthOptionModel::where('mental_health_question_id', $questionId)
+                        ->where('value', $value)
+                        ->first();
+
+                    if ($option) {
+                        MentalHealthAnswerModel::create([
+                            'mental_health_result_id' => $result->id,
+                            'mental_health_question_id' => $questionId,
+                            'mental_health_option_id' => $option->id,
+                        ]);
+
+                        $totalScore += $option->value;
+                    }
+                }
+            }
+
+            $result->update([
+                'total_score' => $totalScore,
+            ]);
+
+            return redirect()->route('mahasiswa.test-kesehatan-mental')->with('success_message_update', 'Data Test Berhasil Di Submit!');
+        } catch (\Exception $e) {
+
+            return redirect()->back()->with('error', 'An error occurred while submitting the test. Please try again.');
+        }
     }
 
 }
